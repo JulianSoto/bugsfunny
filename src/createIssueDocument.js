c7f38de9346f219379dbd291d92fea6573a530f5
juliansoto <juliansotoponce@gmail.com>
2021-04-19 23:49:26 -0700

    use promises for file system

--- src/createIssueDocument.js
+++ src/createIssueDocument.js
@@ -1,27 +1,14 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const generateIssueFilename = require('./generateIssueFilename');
 
-module.exports = (data) => {
-  return new Promise((resolve, reject) => {
-    try {
-      const fileName = generateIssueFilename(data.title || '');
-      const jsonStringData = JSON.stringify(data, null, 2);
-      const filePath = path.join(process.cwd(), '.bugsfunny', fileName);
+module.exports = async (data) => {
+  const fileName = generateIssueFilename(data.title || '');
+  const jsonStringData = JSON.stringify(data, null, 2);
+  const filePath = path.join(process.cwd(), '.bugsfunny', fileName);
 
-      fs.mkdir(
-        path.join(process.cwd(), '.bugsfunny'),
-        { recursive: true },
-        (err) => {
-          if (err) throw err;
-          fs.writeFile(filePath, jsonStringData, (err) => {
-            if (err) throw err;
-            resolve(fileName);
-          });
-        }
-      );
-    } catch (err) {
-      reject(err);
-    }
-  });
+  await fs.mkdir(path.join(process.cwd(), '.bugsfunny'), { recursive: true });
+  await fs.writeFile(filePath, jsonStringData);
+
+  return fileName;
 };
