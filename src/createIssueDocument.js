19da683e7524f444775e469f2819f0d055af4ad4
juliansoto <juliansotoponce@gmail.com>
2021-04-19 23:57:52 -0700

    check if git repo exists in directory

--- src/createIssueDocument.js
+++ src/createIssueDocument.js
@@ -1,13 +1,18 @@
 const fs = require('fs').promises;
 const path = require('path');
+const simpleGit = require('simple-git/promise');
 const generateIssueFilename = require('./generateIssueFilename');
 
 module.exports = async (data) => {
+  const git = simpleGit({
+    baseDir: process.cwd(),
+  });
+  const gitDirectory = await git.revparse('--show-toplevel');
   const fileName = generateIssueFilename(data.title || '');
   const jsonStringData = JSON.stringify(data, null, 2);
-  const filePath = path.join(process.cwd(), '.bugsfunny', fileName);
+  const filePath = path.join(gitDirectory, '.bugsfunny', fileName);
 
-  await fs.mkdir(path.join(process.cwd(), '.bugsfunny'), { recursive: true });
+  await fs.mkdir(path.join(gitDirectory, '.bugsfunny'), { recursive: true });
   await fs.writeFile(filePath, jsonStringData);
 
   return fileName;
