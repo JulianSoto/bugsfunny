369c6154e79dcd085b5f14a5f77763741084e8e3
juliansoto <juliansotoponce@gmail.com>
2021-04-21 00:38:19 -0700

    list issues in console

--- cli.js
+++ cli.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
-const [,, ...args] = process.argv;
+const path = require('path');
+const fs = require('fs/promises');
+const simpleGit = require('simple-git/promise');
+const { Command } = require('commander');
 
-console.log(`Hello world ${args}`);
+const git = simpleGit();
+const program = new Command();
+
+program
+  .command('list')
+  .description('list all issues')
+  .action(async () => {
+    const issuesDirectory = path.join(
+      await git.revparse('--show-toplevel'),
+      '.bugsfunny'
+    );
+    const files = await fs.readdir(issuesDirectory);
+    const issues = await Promise.all(
+      files.map(async (file) => {
+        const content = await fs.readFile(path.join(issuesDirectory, file));
+        return JSON.parse(content);
+      })
+    );
+
+    console.log(issues);
+  });
+
+program.parse();
