11c75b006dd4d0218cafbe90361e3a1fa75af52a
juliansoto <juliansotoponce@gmail.com>
2021-04-22 17:37:16 -0700

    add cli option to filter issues by status

--- cli.js
+++ cli.js
@@ -5,6 +5,11 @@ const list = require('./src/cli/actions/list');
 
 const program = new Command();
 
-program.command('list').description('list all issues').action(list);
+program
+  .command('list')
+  .description('list all issues')
+  .option('-o, --open', 'list only open issues')
+  .option('-c, --closed', 'list only closed issues')
+  .action(list);
 
 program.parse();
--- src/cli/actions/list.js
+++ src/cli/actions/list.js
@@ -5,19 +5,37 @@ const asTable = require('as-table');
 
 const git = simpleGit();
 
-module.exports = async () => {
+module.exports = async (options) => {
+  const includeAllIssues = !options.open && !options.closed;
+
   const issuesDirectory = path.join(
     await git.revparse('--show-toplevel'),
     '.bugsfunny'
   );
+
   const files = await fs.readdir(issuesDirectory);
+
   const issues = await Promise.all(
     files.map(async (file) => {
       const content = await fs.readFile(path.join(issuesDirectory, file));
-      const parsedContent = JSON.parse(content);
-      return { file, ...parsedContent };
+      const parsedContent = { file, ...JSON.parse(content) };
+
+      switch (parsedContent.status) {
+        case 'open':
+          if (includeAllIssues || options.open) {
+            return parsedContent;
+          }
+          break;
+        case 'closed':
+          if (includeAllIssues || options.closed) {
+            return parsedContent;
+          }
+          break;
+        default:
+          return null;
+      }
     })
   );
 
-  console.log(asTable(issues));
+  console.log(asTable(issues.filter((i) => !!i)));
 };
