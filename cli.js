58289421d9412d1fa7b24c705e716f5fe54f2edf
juliansoto <juliansotoponce@gmail.com>
2021-04-23 00:24:42 -0700

    add cli command to create new issues

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 
 const { Command } = require('commander');
 const list = require('./src/cli/actions/list');
+const create = require('./src/cli/actions/create');
 
 const program = new Command();
 
@@ -11,5 +12,26 @@ program
   .option('-o, --open', 'list only open issues')
   .option('-c, --closed', 'list only closed issues')
   .action(list);
+
+program
+  .command('create <title>')
+  .description('create a new issue')
+  .option('-d, --description <description>', 'description of the issue', '')
+  .option(
+    '-a, --assignedTo <assignedTo>',
+    'assign the issue to some contributor',
+    null
+  )
+  .option(
+    '-s, --status <status>',
+    'status of the issue, either `closed` or `open`',
+    'open'
+  )
+  .option(
+    '-l, --labels <labels...>',
+    'add one or more labels to the issue, separated by spaces',
+    []
+  )
+  .action(create);
 
 program.parse();
