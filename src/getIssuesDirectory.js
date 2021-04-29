6f6e7f8d0256a05c862439f8cb0d087ee280b1d9
juliansoto <juliansotoponce@gmail.com>
2021-04-29 00:07:26 -0700

    change simple git basedir when looking for issues dir

--- src/getIssuesDirectory.js
+++ src/getIssuesDirectory.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const simpleGit = require('simple-git');
-const git = simpleGit();
+
+const git = simpleGit({
+  baseDir: process.cwd(),
+});
 
 module.exports = async () => {
   return path.join(await git.revparse('--show-toplevel'), '.bugsfunny');
